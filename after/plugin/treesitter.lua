856b5004892cf7c7d53d045efda91d3b176eaa77
Jiaqi Wang <w.jiaqi004@gmail.com>
2022-08-18 15:28:11 +0300

    add incremental selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,21 +3,25 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		use_languagetree = true, -- Use this to enable language injection
-		-- custom_captures = {
-		--     -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-		--     ["foo.bar"] = "Identifier",
-		-- },
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
 	},
 	context_commentstring = {
 		enable = true,
 		enable_autocmd = false,
 	},
 	-- indent = {
-	--     enable = true
+	-- 	enable = true,
 	-- },
 	rainbow = {
 		enable = false,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
 		-- colors = {}, -- table of hex strings
